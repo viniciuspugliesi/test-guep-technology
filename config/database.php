75ebd9372c829e3fd5afb8e4e9894ca407902757
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2018-01-15 21:55:01 +0000

    Create config for application

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    | -------------------------------------------------------------------
+    |  CONNECTION MYSQL
+    | -------------------------------------------------------------------
+    |   
+    |  Connection config with MySQL
+    */
+    'host'      => 'localhost',
+    'user'      => 'viniciuspugliesi',
+    'pass'      => '',
+    'database'  => 'test_guep_technology',
+    'charset'   => 'utf8',
+];
