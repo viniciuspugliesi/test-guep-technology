59d3301d3145572ede247713c161dd2feae0d2cd
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2018-01-15 21:55:19 +0000

    Create public path for application

---
+++ public/index.php
@@ -0,0 +1,61 @@
+<?php
+
+register_shutdown_function('error_alert'); 
+function error_alert() {
+    if (!$e = error_get_last()) {
+        return;
+    }
+    
+    if (defined('STDIN')) {
+        return;
+    }
+    
+    var_dump($e['type']);
+    var_dump($e['message']);
+    var_dump($e['file']);
+    var_dump($e['line']);
+    
+    die();
+}
+
+header('Content-Type: text/html; charset=utf-8');
+
+date_default_timezone_set('America/Sao_Paulo');
+
+session_start();
+
+/*
+| ------------------------------------------------------------------------------
+|  Autoload
+| ------------------------------------------------------------------------------
+| 
+| Require vendor autoload
+*/
+require __DIR__ . '/vendor/autoload.php';
+
+/*
+| ------------------------------------------------------------------------------
+|  Instance Route
+| ------------------------------------------------------------------------------
+| 
+| Instance Route class application
+*/
+$route = new \Lib\Routing\Route();
+
+/*
+| ------------------------------------------------------------------------------
+|  Routes
+| ------------------------------------------------------------------------------
+| 
+| Require application routes
+*/
+require __DIR__ . '/app/routes.php';
+
+/*
+| -------------------------------------------------------------------
+|  Run the applicaiton routes
+| -------------------------------------------------------------------
+|
+|  Call the run application for roll in route
+*/
+$route->run();
