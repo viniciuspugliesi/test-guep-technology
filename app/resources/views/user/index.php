f81c93be3015629cb00651e86364376300c682c4
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2018-01-15 22:03:10 +0000

    Create resources application

---
+++ app/resources/views/user/index.php
@@ -0,0 +1,1 @@
+<!DOCTYPE html><html><head><title>Listagem de usuários - Teste Guep</title> <?php include(__DIR__ . '/../includes/head.php') ?></head><body> <?php include(__DIR__ . '/../includes/header.php') ?> <?php include(__DIR__ . '/../includes/footer.php') ?> <?php include(__DIR__ . '/../includes/scripts.php') ?></body></html>
